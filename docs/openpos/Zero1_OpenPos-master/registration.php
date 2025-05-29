baad2ca02b5d7ab723757f1394fe21f33048af72
Arron Moss <arron.moss@zero1.co.uk>
2025-05-29 15:06:33 +0100

    oops

--- docs/openpos/Zero1_OpenPos-master/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-    \Magento\Framework\Component\ComponentRegistrar::register(
-        \Magento\Framework\Component\ComponentRegistrar::MODULE,
-        'Zero1_OpenPos',
-        __DIR__
-    );
